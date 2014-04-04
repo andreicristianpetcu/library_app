1e520ca635d21a2daf550cb89b7bef75c4b62c4a
Cegeka Nocturne 1 <cegekanocturne1@cegeka.com>
2014-04-04 13:52:20 +0300

    Add more e2e scenarios.

--- spa/test/e2e/scenarios.js
+++ spa/test/e2e/scenarios.js
@@ -2,26 +2,34 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('ginkgo admin app', function () {
+describe('Library app', function () {
 
     beforeEach(function () {
         browser().navigateTo('/app/');
     });
 
-    it('should automatically redirect to / when location hash/fragment is empty', function () {
+    it('redirects to / when location hash/fragment is empty', function () {
         expect(browser().location().url()).toBe("/");
     });
 
-    describe('home', function () {
+    describe('home page', function () {
         beforeEach(function () {
             browser().navigateTo('#/');
+            if(element('#logout')) {
+                element('#logout').click();
+            }
         });
 
-        it('should render home when user navigates to /', function () {
+        it('renders "Welcome" and login form', function () {
             expect(element('[ng-view] h1:first').text()).toMatch(/Welcome/);
+            expect(element('#username').text()).not().toBe(null);
+            expect(element('#username').text()).not().toBe(null);
+            expect(element('#login').text()).toBe('Login');
+            expect(element('#logout').text()).toBeNull();
+
         });
 
-        it('redirects to books after login', function () {
+        it('redirects to /books after successful login', function () {
             input('username').enter('admin@mailinator.com');
             input('password').enter('test');
             element("#login").click();
@@ -56,8 +64,10 @@ describe('ginkgo admin app', function () {
             expect(element('[ng-view] h1:first').text()).toMatch(/Books/);
         });
 
-        it('should contain a list of books', function () {
-            expect(element('[ng-view] table th').text()).toMatch("TitleAuthorISBN");
+        it('should contain a table with Title Author ISBN', function () {
+            expect(element('[ng-view] table th').text()).toContain("Title");
+            expect(element('[ng-view] table th').text()).toContain("Author");
+            expect(element('[ng-view] table th').text()).toContain("ISBN");
         });
 
         it('should contain an add book button when user has admin role', function () {
@@ -76,8 +86,28 @@ describe('ginkgo admin app', function () {
             browser().navigateTo('#/add_book');
         });
 
-        it('should display a form', function () {
-            //TODO
+        it('should display a form with title, author and isbn inputs and cancel/addbook buttons', function () {
+            expect(element('#addBookForm').text()).not().toBeNull();
+            expect(element('#inputTitle').text()).not().toBeNull();
+            expect(element('#inputAuthor').text()).not().toBeNull();
+            expect(element('#inputIsbn').text()).not().toBeNull();
+            expect(element('#addBookSubmit').text()).toEqual("Save");
+            expect(element('#addBookCancel').text()).toEqual("Cancel");
+        });
+
+        it('should allow adding a book that is available and can be borrowed', function () {
+            var newIsbn = Math.random() * 1234567890|0;
+            input('book.title').enter('testTitle');
+            input('book.author').enter('testAuthor');
+            input('book.isbn').enter(newIsbn);
+            element("#addBookSubmit").click();
+            expect(browser().location().url()).toBe("/books");
+            expect(element('[ng-view] table tr:last').text()).toContain(newIsbn);
+            expect(element('[ng-view] table tr:last').text()).toContain('testTitle');
+            expect(element('[ng-view] table tr:last').text()).toContain('testAuthor');
+            expect(element('[ng-view] table tr:last').text()).toContain('Available');
+            expect(element('[ng-view] table tr:last').text()).toContain('Borrow');
+            console.log("poc");
         });
 
         it('should post to /book on submit button click', function () {
